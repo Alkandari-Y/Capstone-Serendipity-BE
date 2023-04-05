b3939f152e6b66e41784611337a28d0f76dd2f2c
Alkandari-Y <yayalkandari@gmail.com>
2023-04-05 22:07:06 +0300

    cleaned

--- checkins/views.py
+++ checkins/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from rest_framework.views import Response, status
-from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from checkins import models, serializers
